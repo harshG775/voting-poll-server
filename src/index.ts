1ec51e71b59ee8e811beb6dfd0b809802788059e
Harsh <hgaur491@gmail.com>
2024-06-24 17:26:19 +0530

    //

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import app from "./app";
 import { env } from "./lib/env";
-import { createLogger } from "./utils/logger";
 const port = Number(env.PORT) || 4000;
 (async () => {
     try {
         app.listen(port, () => {
-            createLogger().info(`Server running at http://localhost:${port}`);
+            console.log(`Server running at http://localhost:${port}`);
         });
     } catch (error) {
-        createLogger().error("Failed to start server");
+        console.log("Failed to start server");
         console.log(error);
         process.exit(1);
     }
